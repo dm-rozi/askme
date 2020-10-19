94b3d3c824fe84ff1077a9418eeb36e6496f85ed
dm-rozi <dmitry.rozi@gmail.com>
2020-10-20 01:07:22 +0300

    add length validation for Question model

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,5 +2,6 @@ class Question < ApplicationRecord
 
   belongs_to :user
 
-  validates :text, :user, presence: true
+  validates :user, presence: true
+  validates :text, presence: true, length: { maximum: 255 }
 end
