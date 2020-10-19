56e8cb5c86c8737c8d312106cfbd850560485ada
dm-rozi <dmitry.rozi@gmail.com>
2020-10-20 01:55:04 +0300

    fix codestyle

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   ITERATIONS = 20_000
   DIGEST = OpenSSL::Digest::SHA256.new
 
+  attr_accessor :password
+
   has_many :questions
 
   validates :username,
@@ -15,28 +17,11 @@ class User < ApplicationRecord
     presence: true,
     uniqueness: { case_sensitive: false },
     format: { with: /[\w]+@[\w\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i }
-
-  attr_accessor :password
-
   validates :password, presence: true, on: :create
   validates_confirmation_of :password
 
   before_validation :downcase_username
   before_save :encrypt_password
-
-  def encrypt_password
-    if password.present?
-      self.password_salt = User.hash_to_string(OpenSSL::Random.random_bytes(16))
-      # Создаем хэш пароля — длинная уникальная строка, из которой невозможно
-      # восстановить исходный пароль. Однако, если правильный пароль у нас есть,
-      # мы легко можем получить такую же строку и сравнить её с той, что в базе.
-      self.password_hash = User.hash_to_string(
-        OpenSSL::PKCS5.pbkdf2_hmac(
-          password, password_salt, ITERATIONS, DIGEST.length, DIGEST
-        )
-      )
-    end
-  end
 
   def self.hash_to_string(password_hash)
     password_hash.unpack('H*')[0]
@@ -54,6 +39,22 @@ class User < ApplicationRecord
     )
     return user if user.password_hash == hashed_password
     nil
+  end
+
+  private
+
+  def encrypt_password
+    if password.present?
+      self.password_salt = User.hash_to_string(OpenSSL::Random.random_bytes(16))
+      # Создаем хэш пароля — длинная уникальная строка, из которой невозможно
+      # восстановить исходный пароль. Однако, если правильный пароль у нас есть,
+      # мы легко можем получить такую же строку и сравнить её с той, что в базе.
+      self.password_hash = User.hash_to_string(
+          OpenSSL::PKCS5.pbkdf2_hmac(
+              password, password_salt, ITERATIONS, DIGEST.length, DIGEST
+          )
+      )
+    end
   end
 
   def downcase_username
