e426328a2ba9d5d467700fc3685a77ab5f45b2b1
dm-rozi <dmitry.rozi@gmail.com>
2020-10-20 01:05:57 +0300

    add email username validations for User model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,5 @@ class UsersController < ApplicationController
 
   def show
     @hello = 'Hello, world! One more time!'
-    @user = User.all.map(&:username)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,23 @@
 require 'openssl'
 
-
 class User < ApplicationRecord
   ITERATIONS = 20_000
   DIGEST = OpenSSL::Digest::SHA256.new
 
   has_many :questions
 
-  validates :email, :username, presence: true
-  validates :email, :username, uniqueness: { case_sensitive: false }
+  validates :username,
+    presence: true,
+    uniqueness: { case_sensitive: false },
+    length: { maximum: 40 },
+    format: { without: /\W+/ }
+  validates :email,
+    presence: true,
+    uniqueness: { case_sensitive: false },
+    format: { with: /[\w]+@[\w\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i }
 
   attr_accessor :password
+
   validates :password, presence: true, on: :create
   validates_confirmation_of :password
 
@@ -18,23 +25,15 @@ class User < ApplicationRecord
 
   def encrypt_password
     if password.present?
-      # Создаем т.н. «соль» — случайная строка, усложняющая задачу хакерам по
-      # взлому пароля, даже если у них окажется наша БД.
-      #У каждого юзера своя «соль», это значит, что если подобрать перебором пароль
-      # одного юзера, нельзя разгадать, по какому принципу
-      # зашифрованы пароли остальных пользователей
       self.password_salt = User.hash_to_string(OpenSSL::Random.random_bytes(16))
-
       # Создаем хэш пароля — длинная уникальная строка, из которой невозможно
       # восстановить исходный пароль. Однако, если правильный пароль у нас есть,
       # мы легко можем получить такую же строку и сравнить её с той, что в базе.
       self.password_hash = User.hash_to_string(
-          OpenSSL::PKCS5.pbkdf2_hmac(
-              password, password_salt, ITERATIONS, DIGEST.length, DIGEST
-          )
+        OpenSSL::PKCS5.pbkdf2_hmac(
+          password, password_salt, ITERATIONS, DIGEST.length, DIGEST
+        )
       )
-
-      # Оба поля попадут в базу при сохранении (save).
     end
   end
 
