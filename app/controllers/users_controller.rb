95aeb1d479f98a63c4c2034cbfbc92b89163b8d8
dm-rozi <dmitry.rozi@gmail.com>
2020-10-28 00:43:14 +0300

    update user_helper.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,7 @@
 class UsersController < ApplicationController
 
   def index
-    @users = [
-      User.new(
-        id: 1,
-        name: 'Sponge Bob',
-        username: 'SpongeBob',
-        email: 'sponge@bob.com',
-        password: '12345',
-        avatar_url: 'https://i.ibb.co/sRtBG7G/image.jpg'
-      ),
-      User.new(
-        id: 2,
-        name: 'Squidwart from the Sea',
-        username: 'Squidward',
-        email: 'squidward@bob.com',
-        password: '12345'
-      )
-    ]
+    @users = User.all
   end
 
   def new
@@ -31,12 +15,18 @@ class UsersController < ApplicationController
       username: 'SpongeBob',
       email: 'sponge@bob.com',
       password: '12345',
-      # avatar_url: 'https://i.ibb.co/sRtBG7G/image.jpg'
+      avatar_url: 'https://i.ibb.co/sRtBG7G/image.jpg'
     )
 
     @questions = [
       Question.new(text: 'Успеешь?', created_at: Date.parse('25.10.2020')),
       Question.new(text: 'Когда все сделать?', created_at: Date.parse('26.10.2020')),
+      Question.new(text: 'Когда все это сделать?', created_at: Date.parse('26.10.2020')),
+      Question.new(text: 'Ну привет?', created_at: Date.parse('27.10.2020')),
+      Question.new(text: 'Когда все это сделать?', created_at: Date.parse('26.10.2020')),
+      Question.new(text: 'Ну привет?', created_at: Date.parse('27.10.2020')),
+      Question.new(text: 'Когда все это сделать?', created_at: Date.parse('26.10.2020')),
+      Question.new(text: 'Ну привет?', created_at: Date.parse('27.10.2020')),
       Question.new(text: 'Когда все это сделать?', created_at: Date.parse('26.10.2020')),
       Question.new(text: 'Ну привет?', created_at: Date.parse('27.10.2020')),
       Question.new(text: 'Почитаешь доки?', created_at: Date.parse('27.10.2020'))
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,17 +3,17 @@ module UsersHelper
   def questions_count_form(number, vopros, voprosa, voprosov, with_number = false)
     remainder10 = number.to_i % 10
     remainder100 = number.to_i % 100
-    prefix = ""
-    prefix = "#{number} " if with_number
 
-    if (11..14).include?(remainder100)
-      return "#{prefix}#{voprosov}"
-    elsif  remainder10 == 1
-      return "#{prefix}#{vopros}"
-    elsif (2..4).include?(remainder10)
-      return "#{prefix}#{voprosa}"
-    else
-      return "#{prefix}#{voprosov}"
-    end
+    prefix = with_number ? "#{number} " : ""
+
+    return prefix + voprosov if (11..14).include?(remainder100)
+
+    word = case remainder10
+           when 1 then vopros
+           when 2..4 then voprosa
+           else
+             voprosov
+           end
+    prefix + word
   end
 end
