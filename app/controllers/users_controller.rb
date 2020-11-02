22caca9547084de3f8e9e22e7f9842214906bac0
dm-rozi <dmitry.rozi@gmail.com>
2020-11-03 00:36:36 +0300

    add cancel button on edit_view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,38 +1,56 @@
 class UsersController < ApplicationController
 
+  before_action :load_user, except: [:index, :create, :new]
+
   def index
     @users = User.all
   end
 
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to root_url, notice: 'Пользователь успешно зарегистрирован!'
+    else
+      render 'new'
+    end
   end
 
   def edit
   end
 
   def show
-    @user = User.new(
-      username: 'SpongeBob',
-      email: 'sponge@bob.com',
-      password: '12345',
-      avatar_url: 'https://i.ibb.co/sRtBG7G/image.jpg'
-    )
-
-    @questions = [
-      Question.new(text: 'Успеешь?', created_at: Date.parse('25.10.2020')),
-      Question.new(text: 'Когда все сделать?', created_at: Date.parse('26.10.2020')),
-      Question.new(text: 'Когда все это сделать?', created_at: Date.parse('26.10.2020')),
-      Question.new(text: 'Ну привет?', created_at: Date.parse('27.10.2020')),
-      Question.new(text: 'Когда все это сделать?', created_at: Date.parse('26.10.2020')),
-      Question.new(text: 'Ну привет?', created_at: Date.parse('27.10.2020')),
-      Question.new(text: 'Когда все это сделать?', created_at: Date.parse('26.10.2020')),
-      Question.new(text: 'Ну привет?', created_at: Date.parse('27.10.2020')),
-      Question.new(text: 'Когда все это сделать?', created_at: Date.parse('26.10.2020')),
-      Question.new(text: 'Ну привет?', created_at: Date.parse('27.10.2020')),
-      Question.new(text: 'Почитаешь доки?', created_at: Date.parse('27.10.2020'))
-    ]
-
-    @new_question = Question.new
+    @questions = @user.questions.order(created_at: :desc)
     @questions_count = @questions.size
+
+    # Для формы нового вопроса создаём заготовку, вызывая build у результата вызова метода @user.questions.
+    @new_question = @user.questions.build
+    # @new_question = Question.new
+  end
+
+  def update
+    # пытаемся обновить юзера
+    if @user.update(user_params)
+      # Если получилось, отправляем пользователя на его страницу с сообщением
+      redirect_to user_path(@user), notice: 'Данные обновлены'
+    else
+      # Если не получилось, как и в create, рисуем страницу редактирования
+      # пользователя со списком ошибок
+      render 'edit'
+    end
+  end
+
+  private
+
+  def load_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :name, :username, :avatar_url)
   end
 end
