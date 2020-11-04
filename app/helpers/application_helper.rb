576e7255fbe67c705ae2abd81a4e08309ab8a51b
dm-rozi <dmitry.rozi@gmail.com>
2020-11-05 01:26:56 +0300

    add helper for background color change

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,8 @@ module ApplicationHelper
   def fa_icon(icon_class)
     content_tag 'span', '', class: "fa fa-#{icon_class}"
   end
+
+  def user_backcolor(user)
+    user.background_color.present? ? user.background_color : (user.background_color = "#005A55")
+  end
 end
